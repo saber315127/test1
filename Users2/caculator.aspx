adf2ff847b64249d48b85f8c64e753f5114da58f
saber315127 <37133549+saber315127@users.noreply.github.com>
2018-12-24 15:12:35 +0800

    Add files via upload

---
+++ Users2/caculator.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="caculator.aspx.cs" Inherits="caculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:TextBox ID="TextBox1" runat="server" Width="146px"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="1" Width="46px" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="2" Width="46px" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="3" Width="46px" />
+        <asp:Button ID="Button15" runat="server" OnClick="Button15_Click" Text="AC" />
+        <br />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="4" Width="46px" />
+        <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="5" Width="46px" />
+        <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="6" Width="46px" />
+        <asp:Button ID="Button11" runat="server" OnClick="Button11_Click" Text="+" Width="31px" />
+        <br />
+        <asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="7" Width="46px" />
+        <asp:Button ID="Button8" runat="server" OnClick="Button8_Click" style="height: 21px" Text="8" Width="46px" />
+        <asp:Button ID="Button9" runat="server" OnClick="Button9_Click" Text="9" Width="46px" />
+        <asp:Button ID="Button12" runat="server" OnClick="Button12_Click" Text="-" Width="31px" />
+        <br />
+        <asp:Button ID="Button13" runat="server" OnClick="Button13_Click" Text="*" Width="46px" />
+        <asp:Button ID="Button10" runat="server" OnClick="Button10_Click" Text="0" Width="46px" />
+        <asp:Button ID="Button14" runat="server" OnClick="Button14_Click" Text="/" Width="46px" />
+        <asp:Button ID="Button16" runat="server" OnClick="Button16_Click" Text="=" Width="32px" />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Users2/caculator.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+public partial class caculator : System.Web.UI.Page
+{
+    public class Global
+
+    {
+        public static string text = "";
+        public static double total;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Global.text += 1;
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Global.text += 2;
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Global.text += "3";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        Global.text += "4";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        Global.text += "5";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        Global.text += "6";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        Global.text += "7";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        Global.text += "8";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        Global.text += "9";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button10_Click(object sender, EventArgs e)
+    {
+        Global.text += "0";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button11_Click(object sender, EventArgs e)
+    {
+        Global.text += "+";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button12_Click(object sender, EventArgs e)
+    {
+        Global.text += "-";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button13_Click(object sender, EventArgs e)
+    {
+        Global.text += "*";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button14_Click(object sender, EventArgs e)
+    {
+        Global.text += "/";
+        TextBox1.Text = Global.text;
+    }
+
+    protected void Button15_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text = Convert.ToString("0");
+        Global.text = "";
+    }
+
+    protected void Button16_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text = Convert.ToString(Global.text);
+        Global.text = "";
+    }
+}
